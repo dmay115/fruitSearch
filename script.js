0e891f082f5f9a32ab773976b84a200f4d965da0
dmay <dmay115@gmail.com>
2023-05-30 09:51:24 -0500

    update after code review

--- script.js
+++ script.js
@@ -2,42 +2,38 @@ const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
-let typed = ''
-const included = new Set();
 function search(str) {
+	let results = [];
 	for (let strings of fruit){
 		if (strings.toLowerCase().includes(str)){
-		included.add(strings)
+		results.push(strings);
 		}
 	}
-	results = included
-
-	return showSuggestions(results);
+	return showSuggestions(results, str);
 }
 
-function searchHandler(e) {
-	if (e){
-		typed = input.value
-	}
-	included.clear()
-	suggestions.innerText = ''
-	search(typed)
+function searchHandler() {
+	let searchStr = input.value.toLowerCase();
+	results = [];
+	suggestions.innerText = '';
+	search(searchStr);
 }
 
-function showSuggestions(results) {
-	if (typed == ""){
-		included.clear()
+function showSuggestions(arr, searched) {
+	if (input.value === ''){
+		search();
 	}
-	for (let str of results){
+	for (let str of arr){
 		const item = document.createElement('li');
-		item.innerText = str
-		suggestions.appendChild(item)
+		let bolded = str.replace(searched, `<b>${searched}</b>`);
+		item.innerHTML = bolded;
+		suggestions.appendChild(item);
 	}
 }
 
 function useSuggestion(e) {
-	input.value = e.target.innerText
-	suggestions.innerText = ''
+	input.value = e.target.innerText;
+	suggestions.innerText = '';
 }
 
 input.addEventListener('keyup', searchHandler);
